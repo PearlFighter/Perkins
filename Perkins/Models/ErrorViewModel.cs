4da3e823cb6315772cad633a2df822c04ff3b3c1
KO software <kosoftware@hotmail.com>
2020-10-20 09:57:27 -0400

    Initial checking

---
+++ Perkins/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Perkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
